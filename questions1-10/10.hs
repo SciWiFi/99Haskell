91b78fde1aeb2e58cd7aafcb7916c8cbb6becdc7
SciWiFi <SciWiFi@users.noreply.github.com>
2018-09-03 16:26:44 +0200

    9 and 10 implemented

--- questions1-10/10.hs
+++ questions1-10/10.hs
@@ -2,3 +2,9 @@ pack :: Eq a => [a] -> [[a]]
 pack [] = []
 pack xs = [fst a] ++ pack (snd a)
     where a = span (== head xs) xs
+
+encode :: Eq a => [a] -> [(Int, a)]
+encode xs = map f (pack xs)
+
+f :: [a] -> (Int, a)
+f x = (length x, head x)
